6bb342243eb2fad4e823f31b8122a2fed74c874d
blue <midnight@nightmarish.blue>
2024-11-12 15:47:07 +0000

    Give client default constructor

--- src/ChessBackend/Client.ts
+++ src/ChessBackend/Client.ts
@@ -1,8 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class Client {
     public readonly Id: string;
     public Name?: string;
 
-    constructor(id: string, name?: string) {
+    constructor(id: string = uuidv4(), name?: string) {
         this.Id = id;
         this.Name = name;
     }

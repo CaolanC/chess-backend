f12f53082e6ee7c383533e5157ab33c5208f9fde
blue <midnight@nightmarish.blue>
2024-11-13 15:34:50 +0000

    Add router for room endpoints

---
+++ src/rooms.ts
@@ -0,0 +1,44 @@
+import Room from './ChessBackend/Room';
+import RoomManager from './ChessBackend/RoomManager';
+import Client from './ChessBackend/Client';
+import { publicDir } from './constants';
+
+import express, { Request, Response, Router, NextFunction } from "express";
+import path from 'path';
+
+export const roomManager = new RoomManager();
+const rooms: Router = express.Router({mergeParams: true});
+rooms.use(roomExists);
+rooms.use(inRoom);
+
+declare module 'express' {
+    interface Request {
+        room?: Room;
+    }
+}
+
+export function roomExists(req: Request, res: Response, next: NextFunction): void {
+    const room: Room | undefined = roomManager.getRoom(req.params.room_id);
+
+    if (!room) {
+        res.status(404).send('Room not found');
+        return;
+    }
+    req.room = room;
+    next();
+}
+
+export function inRoom(req: Request, res: Response, next: NextFunction): void {
+    const player: Client = req.session!.user;
+    if (!req.room?.hasPlayer(player.Id)) {
+        res.status(403).send("You're not part of this game");
+        return;
+    }
+    next();
+}
+
+rooms.get('/', (req: express.Request, res: express.Response) => {
+    res.sendFile(path.join(publicDir, 'game.html'));
+});
+
+export default rooms;
